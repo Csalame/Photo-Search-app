8c61629b19e1b753c0d99a15f81a20abd13613cb
Carlos Salame <you@yourdomain.com>
2017-12-26 23:45:06 -0500

    1st commi

--- Photo Search app/ViewController.swift
+++ Photo Search app/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,  UISearchBarDelegate {
+    
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchFlickrBy("dogs")
+         }
+    
+    func searchFlickrBy(_ searchString:String) {
+        let manager = AFHTTPSessionManager()
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "216b4e4675d45c964aa116bc4533df61",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": searchString,
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject as? [String: AnyObject] {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            if let photos = (responseObject as AnyObject)["photos"] as? [String: AnyObject] {
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+                                    self.scrollView.contentSize = CGSize(width: 320, height: 320 * CGFloat(photoArray.count))
+                                    for (i,photoDictionary) in photoArray.enumerated() {
+                                        if let imageURLString = photoDictionary["url_m"] as? String {
+                                            let imageView = UIImageView(frame: CGRect(x:0, y:320*CGFloat(i), width:320, height:320))
+                                            if let url = URL(string: imageURLString) {
+                                                imageView.setImageWith(url)
+                                                self.scrollView.addSubview(imageView)
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchFlickrBy(searchText)
+        }
     }
-
-
 }
-
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Photo Search app
+//
+//  Created by Carlos Salame on 12/21/17.
+//  Copyright © 2017 Carlos Salame. All rights reserved.
+//
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIImageView+AFNetworking.h"
+
+#endif /* bridge_h */
